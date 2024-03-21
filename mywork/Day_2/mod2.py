2fa5b7bf2fe059b56127da4104ce86c672a57bfc
BhuvaneshwariS <bhuvanasankar11@gmail.com>
2024-03-21 18:25:30 +0530

    Add files via upload

---
+++ mywork/Day_2/mod2.py
@@ -0,0 +1,9 @@
+def function1(num):
+    """This is a method"""
+    return num**2
+def function2(num):
+    """This is a method"""
+    return num**2
+x=6
+def function3():
+    return "Hello"
---
+++ mywork/Day_4/fix.py
@@ -0,0 +1,7 @@
+hotel_name='ABCD hotel'
+break_fast={1:'Idly',2:'Poori',3:'Pongal',4:'Dosa',5:'Vada'}
+break_fast1={1:30,2:50,3:70,4:50,5:10}
+lunch={1:'Tomato rice',2:'Curd rice',3:'Sambar',4:'Parotta',5:'Chicken'}
+lunch1={1:70,2:100,3:70,4:50,5:70}
+dinner={1:'Idly',2:'Poori',3:'Pongal',4:'Dosa',5:'Vada'}
+dinner1={1:30,2:50,3:70,4:50,5:10}
